da3b1947bbd9745bfe063f1f4574a635d7c12a6f
Jose Uriel <uri.rm.45@gmail.com>
2026-09-16 12:19:50 -0600

    POT - Adicion de modulo de campaña activa

--- assets/js/script_tailwind_config.js
+++ assets/js/script_tailwind_config.js
@@ -6,9 +6,29 @@
                     brandPurpleDark: '#2D2070',
                     brandOrange: '#FB6E1E',
                     brandLime: '#D6FF3F',
+                    paper: '#F8F3FC',
+                    indigo: {
+                        DEFAULT: '#4B2E83',
+                        dark: '#372163',
+                        light: '#6B4BAA',
+                    },
+                    orange: {
+                        DEFAULT: '#FF6A00',
+                        dark: '#DB5A00',
+                        light: '#FF9142',
+                    },
+                    wine: '#7B1E3A',
+                    lime: '#A8E63E',
+                    skyblue: '#BDE4F9',
+                    lavender: '#E6D6F7',
+                    ink: '#2A2340',
+                    mist: '#8B81A6',
+                    faint: '#9C93B5',
                     },
                     fontFamily: {
-                    nav: ['Arial Black', 'Arial', 'sans-serif'],
+                        nav: ['Arial Black', 'Arial', 'sans-serif'],
+                        serif: ['Fraunces', 'serif'],
+                        sans: ['Work Sans', 'sans-serif'],
                     }
                 }
                 }
